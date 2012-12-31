8c65ef9f4615484544b86cd4819ba46960719dc2
Massimo Maino <maintux@gmail.com>
2012-12-31 18:05:39 +0100

    Fix json encoding

--- test/sns_bounce_request.rb
+++ test/sns_bounce_request.rb
@@ -3,16 +3,17 @@ require './lib/ses_proxy/conf'
 
 uri = URI('http://localhost:9292/')
 req = Net::HTTP::Post.new(uri.path)
+msg = %Q{{"notificationType":"Bounce","bounce":{"reportingMTA":"dns; a192-11.smtp-out.amazonses.com","bounceType":"Permanent","bouncedRecipients":[{"emailAddress":"recipient1@example.com","status":"5.0.0","diagnosticCode":"smtp; 5.1.0 - Unknown address error 550-'Requested action not taken: mailbox unavailable' (delivery attempts: 0)","action":"failed"}],"bounceSubType":"General","timestamp":"2012-12-29T14:56:01.000Z","feedbackId":"0000013be729794c-d792580c-51c7-11e2-8222-7deccfe1af64-000000"},"mail":{"timestamp":"2012-12-29T14:55:51.000Z","source":"no-reply@example.com","messageId":"0000013be729724c-90281fd0-8cac-42c4-9710-f20d04b14b86-000000","destination":["recipient1@example.com"]}}}
 req.body = <<-BODY
 {
   "Type" : "Notification",
   "MessageId" : "cc748646-68d6-467a-8970-35173aa51fd5",
   "TopicArn" : "#{SesProxy::Conf.get[:test][:topic_arn]}",
-  "Message" : "{\"notificationType\":\"Bounce\",\"bounce\":{\"reportingMTA\":\"dns; a192-11.smtp-out.amazonses.com\",\"bounceType\":\"Permanent\",\"bouncedRecipients\":[{\"emailAddress\":\"recipient1@example.com\",\"status\":\"5.0.0\",\"diagnosticCode\":\"smtp; 5.1.0 - Unknown address error 550-'Requested action not taken: mailbox unavailable' (delivery attempts: 0)\",\"action\":\"failed\"}],\"bounceSubType\":\"General\",\"timestamp\":\"2012-12-29T14:56:01.000Z\",\"feedbackId\":\"0000013be729794c-d792580c-51c7-11e2-8222-7deccfe1af64-000000\"},\"mail\":{\"timestamp\":\"2012-12-29T14:55:51.000Z\",\"source\":\"no-reply@example.com\",\"messageId\":\"0000013be729724c-90281fd0-8cac-42c4-9710-f20d04b14b86-000000\",\"destination\":[\"recipient1@example.com\"]}}\n",
+  "Message" : "#{msg.gsub('"','\"')}",
   "Timestamp" : "2012-12-29T15:11:21.353Z",
   "SignatureVersion" : "1",
   "Signature" : "",
-  "SigningCertURL" : "",
+  "SigningCertURL" : ""
 }
 BODY
 req.content_type = 'text/plain; charset=UTF-8'
